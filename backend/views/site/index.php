af5a8e9b5ea649be3938eb178117ab02ecce1418
86688 <oa1219@126.com>
2019-01-23 09:28:28 +0800

    Update index.php

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -14,6 +14,7 @@ $this->title = '后台管理系统';
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
 
+    
     <div class="body-content">
 
         <div class="row">
