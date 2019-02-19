cf7c3f246a8b48b9254cf11bba9540e716c2f1d6
86688 <oa1219@126.com>
2019-02-19 15:39:12 +0800

    更新

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -2,12 +2,6 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
-use common\models\Dept;
-use common\models\Title;
-use yii\helpers\Url;
-use yii\widgets\ActiveForm;
-use common\models\User;
-use common\models\Place;
 
 
 /* @var $this yii\web\View */
@@ -17,79 +11,74 @@ use common\models\Place;
 $this->title = '人员';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-    <div class="user-index table table-hover">
+<div class="user-index table table-hover">
+    <!--   标签  -->
+    <p>
+        <?= Html::a('新入人员', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('部门管理', ['dept/index'], ['class' => 'btn btn-success']) ?>
+    </p>
 
-        <p>
-            <?= Html::a('新入人员', ['create'], ['class' => 'btn btn-success']) ?>
-            <?= Html::a('部门管理', ['dept/index'], ['class' => 'btn btn-success']) ?>
-        </p>
+    <?= GridView::widget([
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'columns' => [
+        'user_name',
+        'sex',
+        'tel',
+        'email:email',
 
-        <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            'user_name',
-            'sex',
-            'tel',
-            'email:email',
-
-//            ['attribute'=>'place_id',
-//                'label'=>'地点',
-//                'value'=>'place.place',
-//            ],
-
-            ['attribute'=>'status_id',
-                'label'=>'状态',
-                'value'=>'status.status_name',
-            ],
-            ['attribute'=>'dept_id',
-                'label'=>'部门',
-                'value'=>'dept.dept_name',
-            ],
-            ['attribute'=>'title_id',
-                'label'=>'状态',
-                'value'=>'title.title_name',
-            ],
-            [
-                'attribute' => 'crt_start',
-                'format' => ['date', 'php:Y-m-d']
-            ],
-            [
-                'attribute' => 'crt_end',
-                'format' => ['date', 'php:Y-m-d']
-            ],
-            'ctr_num',
-            //动作按钮
-            ['class' => 'yii\grid\ActionColumn',
-                'template'=>'{view} {update} {delete} {resetpwd} {privilege}',
-                'buttons'=>[
-                    //$url 是列为按钮创建的URL，
-                    //$model是当前要渲染的模型对象，
-                    //$key 是在数据提供者数组中模型的键
-                    'resetpwd'=>function($url,$model,$key)
-                    {
-                        $options=[
-                            'title'=>Yii::t('yii','重置密码'),
-                            'aria-label'=>Yii::t('yii','重置密码'),
-                            'data-pjax'=>'0',
-                            'data-confirm' => Yii::t('yii', '确认重置吗?'),
-
-                        ];
-                        return Html::a('<span class="glyphicon glyphicon-lock"></span>',['user/resetpwd', 'id' => $key],$options);
-                    },
-
-                    'privilege'=>function($url,$model,$key)
-                    {
-                        $options=[
-                            'title'=>Yii::t('yii','权限'),
-                            'aria-label'=>Yii::t('yii','权限'),
-                            'data-pjax'=>'0',
-                        ];
-                        return Html::a('<span class="glyphicon glyphicon-user"></span>',$url,$options);
-                    },
-
-                ],
+        ['attribute'=>'status_id',
+            'label'=>'状态',
+            'value'=>'status.status_name',
         ],
+        ['attribute'=>'dept_id',
+            'label'=>'部门',
+            'value'=>'dept.dept_name',
+        ],
+        ['attribute'=>'title_id',
+            'label'=>'状态',
+            'value'=>'title.title_name',
+        ],
+        [
+            'attribute' => 'crt_start',
+            'format' => ['date', 'php:Y-m-d']
+        ],
+        [
+            'attribute' => 'crt_end',
+            'format' => ['date', 'php:Y-m-d']
+        ],
+        'ctr_num',
+        //动作按钮
+        ['class' => 'yii\grid\ActionColumn',
+            'template'=>'{view} {update} {delete} {resetpwd} {privilege}',
+            'buttons'=>[
+                //$url 是列为按钮创建的URL，
+                //$model是当前要渲染的模型对象，
+                //$key 是在数据提供者数组中模型的键
+                'resetpwd'=>function($url,$model,$key)
+                {
+                    $options=[
+                        'title'=>Yii::t('yii','重置密码'),
+                        'aria-label'=>Yii::t('yii','重置密码'),
+                        'data-pjax'=>'0',
+                        'data-confirm' => Yii::t('yii', '确认重置吗?'),
+
+                    ];
+                    return Html::a('<span class="glyphicon glyphicon-lock"></span>',['user/resetpwd', 'id' => $key],$options);
+                },
+
+                'privilege'=>function($url,$model,$key)
+                {
+                    $options=[
+                        'title'=>Yii::t('yii','权限'),
+                        'aria-label'=>Yii::t('yii','权限'),
+                        'data-pjax'=>'0',
+                    ];
+                    return Html::a('<span class="glyphicon glyphicon-user"></span>',$url,$options);
+                },
+
             ],
-    ]); ?>
+    ],
+        ],
+]); ?>
 </div>
