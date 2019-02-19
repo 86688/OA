f6cfef5e47ad9031960e25984ccc2257344b9596
86688 <oa1219@126.com>
2019-02-19 11:02:23 +0800

    页面修改

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -4,10 +4,17 @@
 
 $this->title = '后台管理系统';
 ?>
-<div class="site-index">
+<div class="container">
 
-    <div class="jumbotron">
-        <p><a class="btn btn-lg btn-success" href="http://admin.oa.com/index.php?r=user">人事模块</a></p>
+    <div class="row jumbotron">
+        <p class="btn btn-lg btn-success col-md-4"><a href="http://admin.oa.com/index.php?r=user">人事模块</a></p>
+        <p class="btn btn-lg btn-success col-md-4 col-md-offset-2"><a href="http://admin.oa.com/index.php?r=user">人事模块</a></p>
+    </div>
+
+
+    <div class="row jumbotron">
+        <p class="btn btn-lg btn-success col-md-4"><a href="http://admin.oa.com/index.php?r=user">人事模块</a></p>
+        <p class="btn btn-lg btn-success col-md-4 col-md-offset-2"><a href="http://admin.oa.com/index.php?r=user">人事模块</a></p>
     </div>
 
 </div>
