e2f92d354fce6c6e7690964bf350c73c39ccd892
86688 <oa1219@126.com>
2019-02-27 16:42:46 +0800

    更新

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -8,11 +8,11 @@ $this->title = '后台管理系统';
 
     <div class="row jumbotron">
         <a href="http://admin.oa.com/index.php?r=user"><p class="btn btn-lg btn-success col-md-4">人事</p></a>
-        <a href="http://admin.oa.com/index.php?r=project"><p class="btn btn-lg btn-success col-md-4 col-md-offset-2">项目</p></a>
+        <a href="http://admin.oa.com/index.php?r=project"><p class="btn btn-lg btn-success col-md-4 col-md-offset-1">项目</p></a>
         <a href="http://admin.oa.com/index.php?r=finance"><p class="btn btn-lg btn-success col-md-4">财务</p></a>
-        <a href="http://admin.oa.com/index.php?r=vendor"><p class="btn btn-lg btn-success col-md-4 col-md-offset-2">供应商</p></a>
+        <a href="http://admin.oa.com/index.php?r=vendor"><p class="btn btn-lg btn-success col-md-4 col-md-offset-1">供应商</p></a>
         <a href="http://admin.oa.com/index.php?r=client"><p class="btn btn-lg btn-success col-md-4">客户</p></a>
-        <a href="http://admin.oa.com/index.php?r=vendor"><p class="btn btn-lg btn-success col-md-4 col-md-offset-2">空白</p></a>
+        <a href="http://admin.oa.com/index.php?r=vendor"><p class="btn btn-lg btn-success col-md-4 col-md-offset-1">空白</p></a>
 
     </div>
 
