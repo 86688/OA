ffe77b2d4da1e7f21dcd65b5ef0e9f0b164df477
86688 <oa1219@126.com>
2019-03-01 18:14:32 +0800

    更新

--- frontend/views/emp/index.php
+++ frontend/views/emp/index.php
@@ -22,24 +22,11 @@ $this->params['breadcrumbs'][] = $this->title;
     <!-- 以下为用户的列表   -->
     <?= GridView::widget([
     'dataProvider' => $dataProvider,
-    //'filterModel' => $searchModel,
     'columns' => [
         'user_name',
-//        ['attribute'=>'sex',
-//            'label'=>'性别',
-//            'filter'=>User::allSex(),
-//        ],
-        'tel',
-//        'email:email',
 
-//        ['attribute'=>'status_id',
-//            'label'=>'状态',
-//            'value'=>'status.status_name',
-//            'filter'=>Status::find()
-//                ->select(['status_name'])
-//         	    ->orderBy('status_id')
-//         	    ->column(),
-//        ],
+        'tel',
+
         ['attribute'=>'dept_id',
             'label'=>'部门',
             'value'=>'dept.dept_name',
@@ -56,48 +43,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 ->orderBy('title_id')
                 ->column(),
         ],
-//        [
-//            'attribute' => 'crt_start',
-//            'format' => ['date', 'php:Y-m-d']
-//        ],
-//        [
-//            'attribute' => 'crt_end',
-//            'format' => ['date', 'php:Y-m-d']
-//        ],
-//        'ctr_num',
 
-
-        //动作按钮
-//        ['class' => 'yii\grid\ActionColumn',
-//            'template'=>'{view}',
-//            'buttons'=>[
-//                //$url 是列为按钮创建的URL，
-//                //$model是当前要渲染的模型对象，
-//                //$key 是在数据提供者数组中模型的键
-//                'resetpwd'=>function($url,$model,$key)
-//                {
-//                    $options=[
-//                        'title'=>Yii::t('yii','重置密码'),
-//                        'aria-label'=>Yii::t('yii','重置密码'),
-//                        'data-pjax'=>'0',
-//                        'data-confirm' => Yii::t('yii', '确认重置吗?'),
-//
-//                    ];
-//                    return Html::a('<span class="glyphicon glyphicon-lock"></span>',['user/resetpwd', 'id' => $key],$options);
-//                },
-//
-//                'privilege'=>function($url,$model,$key)
-//                {
-//                    $options=[
-//                        'title'=>Yii::t('yii','权限'),
-//                        'aria-label'=>Yii::t('yii','权限'),
-//                        'data-pjax'=>'0',
-//                    ];
-//                    return Html::a('<span class="glyphicon glyphicon-user"></span>',$url,$options);
-//                },
-//
-//            ],
-//    ],
         ],
 ]); ?>
 </div>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -7,7 +7,7 @@ $this->title = '后台管理系统';
 <div class="container">
 
     <div class="row jumbotron">
-        <a href="http://admin.oa.com/index.php?r=user"><p class="btn btn-lg btn-success col-md-4">人事</p></a>
+        <a href="http://public.oa.com/index.php?r=emp"><p class="btn btn-lg btn-success col-md-4">人事</p></a>
         <a href="http://admin.oa.com/index.php?r=project"><p class="btn btn-lg btn-success col-md-4 col-md-offset-1">项目</p></a>
         <a href="http://admin.oa.com/index.php?r=finance"><p class="btn btn-lg btn-success col-md-4">财务</p></a>
         <a href="http://admin.oa.com/index.php?r=vendor"><p class="btn btn-lg btn-success col-md-4 col-md-offset-1">供应商</p></a>
